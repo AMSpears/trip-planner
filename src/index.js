015617a17199cba41cfc97abbbfa6f31905c9d6a
Angie Spears <angie.spears809@gmail.com>
2020-03-16 14:37:34 -0400

    update marker

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ const map = new mapboxgl.Map({
   style: "mapbox://styles/mapbox/streets-v11" // mapbox has lots of different map styles available.
 });
 
-const markerDomElm =  document.getElementById('map')
-markerDomElm.style.width = "32px";
-markerDomElm.style.height = "39px";
-markerDomElm.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
-new mapboxgl.Marker(markerDomElm).setLngLat([-74.009, 40.705]).addTo(map);
+const markerDomEl = document.createElement("div");
+markerDomEl.style.width = "32px";
+markerDomEl.style.height = "39px";
+markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map);
